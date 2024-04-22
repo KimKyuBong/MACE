c4b15b9a23f7b2faedbd0d71abcea1c47d540485
KimKyuBong <tfm0405@gmail.com>
2024-04-22 08:21:08 +0000

    START:project

---
+++ mace_project/back/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+app = FastAPI()
+
+from domain.question import question_router
+app = FastAPI()
+origins = [
+"http://127.0.0.1:3000","http://192.168.1.105:3000"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(question_router.router)
